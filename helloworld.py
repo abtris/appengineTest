3abcde810d4b992d8e3881ee00e48a2a86c30f46
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2009-11-10 10:16:57 +0100

    Using forms

--- helloworld.py
+++ helloworld.py
@@ -1,13 +1,31 @@
+import cgi
+
+from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 class MainPage(webapp.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Hello, webapp World!')
+        self.response.out.write("""
+          <html>
+            <body>
+              <form action="/sign" method="post">
+                <div><textarea name="content" rows="3" cols="60"></textarea></div>
+                <div><input type="submit" value="Sign Guestbook"></div>
+              </form>
+            </body>
+          </html>""")
+
+
+class Guestbook(webapp.RequestHandler):
+    def post(self):
+        self.response.out.write('<html><body>You wrote:<pre>')
+        self.response.out.write(cgi.escape(self.request.get('content')))
+        self.response.out.write('</pre></body></html>')
 
 application = webapp.WSGIApplication(
-                                     [('/', MainPage)],
+                                     [('/', MainPage),
+                                      ('/sign', Guestbook)],
                                      debug=True)
 
 def main():
